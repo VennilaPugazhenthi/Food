316ac4ce5361935c93b04c9f0287b5ac5263d477
VennilaPugazhenthi <vennilapugazhenthi@gmail.com>
2021-01-04 13:39:36 -0500

    Add yelp api and search bar

---
+++ src/hooks/useResults.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import yelp from "../api/yelp";
+
+export default () => {
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "glenmoore",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (e) {
+      setErrorMessage("Something went wrong");
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    searchApi("Burger King");
+  }, []);
+
+  return [searchApi, results, errorMessage];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
+import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+  const [searchApi, results, errorMessage] = useResults();
   return (
-    <View>
-      <Text>Search Screen</Text>
+    <View style={styles.background}>
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
+      <ResultsList title="Cost Effective" />
+      <ResultsList title="Bit Pricier" />
+      <ResultsList title="Big Spender" />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "white",
+    flex: 1,
+  },
+});
 
 export default SearchScreen;
